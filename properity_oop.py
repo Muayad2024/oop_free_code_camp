dc68d99acd9e49cf2cd8d411763e4064b2ef85c8
Muayad2024 <muayadtrade@gmail.com>
2026-04-30 05:01:59 +0800

    how to use the deleter properity

--- properity_oop.py
+++ properity_oop.py
@@ -19,7 +19,7 @@ class Circle:
     def radius(self):
         print("Deleting radius...")
         del self._radius
-        
+
 """we use the same function name for the setter and getter
  but if we assign it to value it become setter 
  and if we didin't do it become getter"""
@@ -29,3 +29,14 @@ print('Initial radius:', my_circle.radius) # Initial radius: 3
 
 my_circle.radius = 8
 print('After modifying the radius:', my_circle.radius)
+
+# Delete the radius
+# This calls the deleter
+del my_circle.radius # Deleting radius...
+print("Radius deleted!") # Radius deleted!
+
+# Try to access radius after deletion
+try:
+    print(my_circle.radius)
+except AttributeError as e:
+    print("Error:", e) # Error: 'Circle' object has no attribute '_radius'
